0d87f19c6c3aad59723665e35a609545f83e71cb
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-08-02 19:09:13 +0530

    first commit in this repos

---
+++ methods/post-request.js
@@ -0,0 +1,28 @@
+const crypto = require("crypto");
+const requestBodyparser = require("../util/body-parser");
+const writeTofile = require("../util/write-to-file");
+
+module.exports = async (req, res) => {
+    if (req.url === "/api/movies") {
+        try {
+            let body = await requestBodyparser(req);
+            body.id = crypto.randomUUID();
+            req.movies.push(body);
+            writeTofile(req.movies);
+            res.writeHead(201, {"Content-Type": "application/json"})
+            res.end();
+        }
+        catch (err) {
+            console.log(err);
+            res.writeHead(400, {"Content-Type": "application/json"});
+            res.end(
+                JSON.stringify({
+                    title: "Validation Failed", 
+                    message: "Request body is not valid"
+                }));
+        }
+    }else {
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({title: "Not Found", message: "Route not Found"}))
+    }
+};
